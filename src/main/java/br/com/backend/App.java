dd21ae387084fca7f1cf2f5601649691e6ad0a67
leo-jansen <janseleo@gmail.com>
2021-12-24 13:02:29 -0300

    Quickstart maven project jdk8

---
+++ src/main/java/br/com/backend/App.java
@@ -0,0 +1,17 @@
+package br.com.backend;
+
+/**
+ * Hello world!
+ */
+public final class App {
+  private App() {
+  }
+
+  /**
+   *
+   * @param args The arguments of the program.
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
